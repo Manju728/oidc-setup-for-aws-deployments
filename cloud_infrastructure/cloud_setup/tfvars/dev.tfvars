fe9abc05bbf19d5ef6e43322960a6ee209ac99a3
Ramakoteswara-Rao Mareedu <Ramakoteswara-Rao.Mareedu@partner.bmwgroup.com>
2024-10-14 18:04:36 +0530

    secrets added in

--- cloud_infrastructure/cloud_setup/tfvars/dev.tfvars
+++ cloud_infrastructure/cloud_setup/tfvars/dev.tfvars
@@ -1,2 +1,1 @@
-account_id = "728747466273"
 aws_region = "ap-south-1"
--- cloud_infrastructure/cloud_setup/variables.tf
+++ cloud_infrastructure/cloud_setup/variables.tf
@@ -1,9 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-variable "account_id" {
-  description = "AWS account id"
-}
-
 variable "aws_region" {
   description = "AWS region"
 }
